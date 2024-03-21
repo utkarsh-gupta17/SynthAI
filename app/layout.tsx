dd9f743cc33e1138cea2f601f0fd44f2d13c5128
utkarsh-gupta17 <211220061@nitdelhi.ac.in>
2024-03-22 00:14:33 +0530

    sidebar,dashboard & conversation ui component completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SynthAI",
+  title: "SynthAi",
   description: "SaaS AI Platform",
 };
 
---
+++ components/ui/mobile-sidebar.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Menu } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import Sidebar from "@/components/sidebar"
+import { useEffect, useState } from "react"
+
+const MobileSidebar = () => {
+
+  // for dealing with hydration error that come with this component
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(()=>{
+    setIsMounted(true);
+  },[]);
+  if(!isMounted) return null;
+
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button className="md:hidden" variant="ghost" size="icon">
+          <Menu/>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar/>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default MobileSidebar
